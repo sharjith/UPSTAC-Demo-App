16701a35c019738836892eeb7fcfb2025667a033
Sharjith Naramparambath <sharjith@gmail.com>
2021-06-14 21:39:34 +0530

    Implemented getForTester method of class LabRequestController and methods getForConsultations, getForDoctor, assignForConsultation, and updateConsultation of class ConsultationController

--- UPSTAC-Week3-assignment/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
+++ UPSTAC-Week3-assignment/src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.config.security.UserLoggedInService;
 import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
 import org.upgrad.upstac.testrequests.TestRequestUpdateService;
 import org.upgrad.upstac.testrequests.flow.TestRequestFlowService;
+import org.upgrad.upstac.users.User;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -29,8 +31,6 @@ public class ConsultationController {
     Logger log = LoggerFactory.getLogger(ConsultationController.class);
 
 
-
-
     @Autowired
     private TestRequestUpdateService testRequestUpdateService;
 
@@ -39,16 +39,15 @@ public class ConsultationController {
 
 
     @Autowired
-    TestRequestFlowService  testRequestFlowService;
+    TestRequestFlowService testRequestFlowService;
 
     @Autowired
     private UserLoggedInService userLoggedInService;
 
 
-
     @GetMapping("/in-queue")
     @PreAuthorize("hasAnyRole('DOCTOR')")
-    public List<TestRequest> getForConsultations()  {
+    public List<TestRequest> getForConsultations() {
 
         // Implement this method
 
@@ -58,15 +57,12 @@ public class ConsultationController {
         //return the result
         // For reference check the method getForTests() method from LabRequestController class
 
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
-
-
+        return testRequestQueryService.findBy(RequestStatus.LAB_TEST_COMPLETED);
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('DOCTOR')")
-    public List<TestRequest> getForDoctor()  {
+    public List<TestRequest> getForDoctor() {
 
         //Implement this method
 
@@ -75,14 +71,10 @@ public class ConsultationController {
         //Make use of the findByDoctor() method from testRequestQueryService class to get the list
         // For reference check the method getForTests() method from LabRequestController class
 
-        // replace this line of code with your implementation
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
+        User doctor = userLoggedInService.getLoggedInUser();
 
-
-
-
+        return testRequestQueryService.findByDoctor(doctor);
     }
-
 
 
     @PreAuthorize("hasAnyRole('DOCTOR')")
@@ -97,19 +89,20 @@ public class ConsultationController {
         // return the above created object
         // For reference check the method assignForLabTest() method from LabRequestController class
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
 
-        }catch (AppException e) {
+            User doctor = userLoggedInService.getLoggedInUser();
+
+            return testRequestUpdateService.assignForConsultation(id, doctor);
+
+        } catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
 
 
-
     @PreAuthorize("hasAnyRole('DOCTOR')")
     @PutMapping("/update/{id}")
-    public TestRequest updateConsultation(@PathVariable Long id,@RequestBody CreateConsultationRequest testResult) {
+    public TestRequest updateConsultation(@PathVariable Long id, @RequestBody CreateConsultationRequest testResult) {
 
         // Implement this method
 
@@ -120,17 +113,17 @@ public class ConsultationController {
         // For reference check the method updateLabTest() method from LabRequestController class
 
         try {
-            // replace this line of code with your implementation
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented");
 
+            User doctor = userLoggedInService.getLoggedInUser();
+
+            return testRequestUpdateService.updateConsultation(id, testResult, doctor);
 
         } catch (ConstraintViolationException e) {
             throw asConstraintViolation(e);
-        }catch (AppException e) {
+        } catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
-
 
 
 }
